8e1fcfafa2e47ac566c4eaa7bdcd97ca9940d48b
Meryem Sude ÇETİN <56775168+Sude4@users.noreply.github.com>
2021-04-25 00:41:39 +0300

    Add files via upload

--- oopIntro/ProductManager.java
+++ oopIntro/ProductManager.java
@@ -1,6 +1,7 @@
 package oopIntro;
 
 public class ProductManager {
+	System.out.println("Sepetten çýkarýldý :" + product.name); 
 public void addToCart(Product product) {
 	System.out.println("Sepete eklendi :" + product.name); 
   }
